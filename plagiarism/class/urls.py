0a815fbbb1aa035b88878e466609d854158a187e
Fathima Farhath <farhathali631@gmail.com>
2024-03-18 22:23:00 +0530

    added workspace class and added the functionality for the teacher to add new workspace with a random code

--- plagiarism/class/urls.py
+++ plagiarism/class/urls.py
@@ -3,16 +3,25 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
+    path('base/base', views.class_base, name='class_base'),
+    path('join/',views.join,name='join'),
+
+
     path('',views.index,name='index'),
-    path('base/', views.base, name='base'),
-    path('signup/', views.signup, name = 'signup'),
-    path('login/', views.login, name = 'login'),
-    
     path('student_dashboard/', views.student, name = 'student'),
+    path('create_class/', views.create_class,name='create_class'),
+    path('delete/<uuid:workspace_id>/', views.delete_workspace, name='delete_workspace'),
+    path('workspace/<uuid:workspace_id>/', views.open_workspace, name='open_workspace'),
+    # path('single/', views.single, name='single'),
     path('send_mail/', views.send_mail, name = 'send_mail'),
     path('recieve_mail/',views.recieve_mail,name='recieve_email'),
     path('teacher_dashboard/',views.teacher, name='teacher'),
     
-    path('create_class/', views.create_class,name='create_class'),
     path('student_list/',views.people,name='people'),
+
+
+
+    path('base/', views.base, name='base'),
+    path('signup/', views.signup, name = 'signup'),
+    path('login/', views.login, name = 'login'),
 ]
--- plagiarism/class/views.py
+++ plagiarism/class/views.py
@@ -1,6 +1,50 @@
 from django.shortcuts import render
+from . models import *
+from django.shortcuts import redirect, render
+from django.contrib import messages
+from django.http import Http404
 
 # Create your views here.
+def create_class(request):
+    workspaces=WorkSpace.objects.all()
+    if request.method=="POST":
+        name = request.POST.get('name')
+        detail = request.POST.get('detail')
+        stream = request.POST.get('unit')
+        workspace=WorkSpace(name=name,details=detail,stream=stream)
+        workspace.save()
+        messages.success(request,' Classroom Has Been Created !!')
+        return  redirect('teacher') 
+    else:
+        return render(request,'class/create_class.html') 
+
+def teacher(request):
+    workspaces = WorkSpace.objects.all()
+    if not workspaces:
+        messages.success(request,'No workspce exists !!')
+    return render(request,'dashboard/teacher/teacher.html',{'workspace': workspaces}) 
+    
+def delete_workspace(request, workspace_id):
+    try:
+        workspace = WorkSpace.objects.get(id=workspace_id)
+        workspace.delete()
+        return redirect('teacher')
+    except WorkSpace.DoesNotExist:
+        raise Http404("Workspace does not exist")
+
+
+def open_workspace(request, workspace_id):
+    single=workspace = WorkSpace.objects.filter(id=workspace_id)
+    return render(request,'class/single.html',{'single_workspace':single})
+
+def class_base(request):
+    return render(request,'class/base.html')
+
+
+def join(request):
+    return render(request,'class/single.html')
+
+
 def index(request):
     return render(request,'index.html')
 
@@ -21,12 +65,5 @@ def send_mail(request):
 
 def recieve_mail(request):
     return render(request,'dashboard/student/email.html')
-
-def teacher(request):
-    return render(request,'dashboard/teacher/teacher.html') 
-
-def create_class(request):
-    return render(request,'class/create_class.html') 
-  
 def people(request):
     return render(request,'class/people.html')
