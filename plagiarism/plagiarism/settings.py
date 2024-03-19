d4216b8c7c688e7596b2f53771ddfcbca7e2604d
Fathima Farhath <farhathali631@gmail.com>
2024-03-19 17:19:12 +0530

    All changes updated and resolved the issues resolved the issues

--- plagiarism/plagiarism/settings.py
+++ plagiarism/plagiarism/settings.py
@@ -79,16 +79,10 @@ WSGI_APPLICATION = 'plagiarism.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Plagiarism checker', 
-        'USER': 'postgres',
-        'PASSWORD': '1234',
         'HOST': '127.0.0.1', 
-        'PORT': '5432',
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'Plagiarism_Checker', 
         'USER': 'postgres',
-        'PASSWORD': '631',
-        'PORT': '5432',
+        'PASSWORD': '631'
     }
 }
 
